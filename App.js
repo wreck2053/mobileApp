b4462d4181cf5de973ff801c79f6ca68ab9e085a
Rahul <87407780+wreck2053@users.noreply.github.com>
2025-04-02 15:58:26 +0530

    NEW VERSION - UI updated

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import {
-  createStackNavigator,
-  TransitionPresets,
-} from "@react-navigation/stack";
-import HomeScreen from "./HomeScreen";
-import MyBedroomScreen from "./MyBedroomScreen";
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
+import BedroomScreen from './BedroomScreen';
+import SettingsScreen from './SettingsScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,23 +10,23 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="MyBedroom"
+        initialRouteName="Bedroom"
         screenOptions={{
-          headerStyle: { backgroundColor: "#434343" },
-          headerTintColor: "#f4f4f9",
-          headerTitleStyle: { fontWeight: "bold" },
+          headerShown: false,
+          headerStyle: {backgroundColor: '#434343'},
+          headerTintColor: '#f4f4f9',
+          headerTitleStyle: {fontWeight: 'bold'},
           ...TransitionPresets.SlideFromRightIOS,
-        }}
-      >
+        }}>
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: "Home" }}
+          name="Bedroom"
+          component={BedroomScreen}
+          options={{title: 'Bedroom'}}
         />
         <Stack.Screen
-          name="MyBedroom"
-          component={MyBedroomScreen}
-          options={{ title: "My Bedroom" }}
+          name="Settings"
+          component={SettingsScreen}
+          options={{title: 'Settings'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
